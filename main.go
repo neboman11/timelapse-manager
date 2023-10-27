673beba1ac7c75c9ada470d4189dc6ae5d992dd5
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2023-10-27 16:59:19 -0500

    Added image cleanup, added record to videos table

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strings"
+	"time"
 
 	"github.com/rs/zerolog/log"
 
 	"github.com/neboman11/timelapse-manager/api"
 	_ "github.com/neboman11/timelapse-manager/docs"
+	"github.com/neboman11/timelapse-manager/models"
 )
 
 func main() {
@@ -24,38 +27,66 @@ func main() {
 		log.Fatal().Msg("Invalid port specified.")
 	}
 
-	go encodeInProgress()
-
 	startDaemon(*portPtr)
 }
 
 // Setup IPFS and start listening for requests
 func startDaemon(port int) {
 	open_database()
+
+	go encodeInProgress()
+
 	log.Info().Msg("Ready for requests")
 	api.HandleRequests(port, db)
 }
 
 func encodeInProgress() {
-	baseInProgressFolder := os.Getenv("BASE_INPROGRESS_FOLDER")
-	workingEncodingFolder := path.Join(baseInProgressFolder, "60e849a9-04c9-4edb-9204-d830f64d5cd0")
+	// ticker := time.NewTicker(24 * time.Hour)
+	ticker := time.NewTicker(10 * time.Minute)
 
-	var ffmpegArgs []string
+	for {
+		<-ticker.C
+		var inProgressTimelapses []models.InProgress
+		db.Where(models.InProgress{Status: "InProgress"}).Find(&inProgressTimelapses)
 
-	ffmpegArgs = append(ffmpegArgs, "-f")
-	ffmpegArgs = append(ffmpegArgs, "image2")
-	ffmpegArgs = append(ffmpegArgs, "-r")
-	ffmpegArgs = append(ffmpegArgs, "1")
-	ffmpegArgs = append(ffmpegArgs, "-i")
-	ffmpegArgs = append(ffmpegArgs, path.Join(workingEncodingFolder, "%05d.jpg"))
-	ffmpegArgs = append(ffmpegArgs, path.Join(workingEncodingFolder, "test.avi"))
+		for _, timelapse := range inProgressTimelapses {
+			var ffmpegArgs []string
 
-	cmd := exec.Command("ffmpeg", ffmpegArgs...)
+			ffmpegArgs = append(ffmpegArgs, "-f")
+			ffmpegArgs = append(ffmpegArgs, "image2")
+			ffmpegArgs = append(ffmpegArgs, "-r")
+			ffmpegArgs = append(ffmpegArgs, "1")
+			ffmpegArgs = append(ffmpegArgs, "-i")
+			ffmpegArgs = append(ffmpegArgs, path.Join(timelapse.Folder, "%05d.jpg"))
+			ffmpegArgs = append(ffmpegArgs, path.Join(timelapse.Folder, "complete.mp4"))
 
-	stdout, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Fatal().Err(err)
+			cmd := exec.Command("ffmpeg", ffmpegArgs...)
+
+			stdout, err := cmd.CombinedOutput()
+			if err != nil {
+				log.Error().Err(err)
+			}
+
+			log.Info().Msgf("%s", stdout)
+
+			timelapse.Status = "Complete"
+			db.Save(timelapse)
+
+			var video models.Videos
+			video.Date = time.Now()
+			video.Location = path.Join(timelapse.Folder, "complete.mp4")
+			db.Create(&video)
+
+			files, err := os.ReadDir(timelapse.Folder)
+			if err != nil {
+				log.Error().Err(err)
+			}
+
+			for _, file := range files {
+				if strings.HasSuffix(file.Name(), "jpg") {
+					os.Remove(path.Join(timelapse.Folder, file.Name()))
+				}
+			}
+		}
 	}
-
-	log.Info().Msgf("%s", stdout)
 }
