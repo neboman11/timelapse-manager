80853ab00b3b91e4c44b9b642f09e40e16466d2e
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2023-10-28 15:48:51 -0500

    Added main function and wrapped worker in try catch

--- rpi_upload.py
+++ rpi_upload.py
@@ -13,28 +13,36 @@ picam2.configure(preview_config)
 def take_picture(scheduler):
     scheduler.enter(5, 1, take_picture, (scheduler,))
 
-    picam2.start_preview(Preview.QTGL)
+    try:
+        picam2.start_preview(Preview.QTGL)
 
-    picam2.start()
-    time.sleep(2)
+        picam2.start()
+        time.sleep(2)
 
-    metadata = picam2.capture_file("current_frame.jpg")
-    print(metadata)
+        metadata = picam2.capture_file("current_frame.jpg")
+        print(metadata)
 
-    picam2.close()
+        picam2.close()
 
-    image_content = None
-    with open("current_frame.jpg", "rb") as f:
-        image_content = f.read()
+        image_content = None
+        with open("current_frame.jpg", "rb") as f:
+            image_content = f.read()
 
-    response = requests.post(
-        f"http://timelapse/inprogress/add?id={current_id}", image_content
-    )
+        response = requests.post(
+            f"http://timelapse/inprogress/add?id={current_id}", image_content
+        )
 
-    response.raise_for_status()
-    current_id = response.content.decode()
+        response.raise_for_status()
+        current_id = response.content.decode()
+    except Exception as e:
+        print(f"Failure during image upload: {e}")
 
 
-my_scheduler = sched.scheduler(time.time, time.sleep)
-my_scheduler.enter(5, 1, take_picture, (my_scheduler,))
-my_scheduler.run()
+def main():
+    my_scheduler = sched.scheduler(time.time, time.sleep)
+    my_scheduler.enter(5, 1, take_picture, (my_scheduler,))
+    my_scheduler.run()
+
+
+if __name__ == "__main__":
+    main()
