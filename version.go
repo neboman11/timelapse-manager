ee8c63a14c72fb5bb12a39e59b6ea634d1ff9836
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2023-11-06 09:02:23 -0600

    Bump version

--- version.go
+++ version.go
@@ -4,4 +4,4 @@ package main
 var CurrentCommit string
 
 // CurrentVersionNumber is the current application's version literal
-const CurrentVersionNumber = "0.1.5"
+const CurrentVersionNumber = "0.1.6"
